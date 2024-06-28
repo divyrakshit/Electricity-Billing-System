192be6b65f7b83f7e2d4ea6d9ed674cc9f21f7b8
divyrakshit <divyrakshit07@gmail.com>
2024-06-28 12:49:07 +0530

    Update CalculateBill.java

--- CalculateBill.java
+++ CalculateBill.java
@@ -12,111 +12,106 @@ import java.sql.ResultSet;
 import java.util.*;
 
 
-public class CalculateBill extends JFrame implements ActionListener {
 
 
 
-    JTextField tfname , tfaddress , tfstate , tfunits , tfemail , tfphone;
-    JButton next,cancel;
-    JLabel lblname , labeladdress;
-    Choice meternumber , cmonth;
+public class CalculateBill extends JFrame implements ActionListener{
+
+    JTextField tfname, tfaddress, tfstate, tfunits, tfemail, tfphone;
+    JButton next, cancel;
+    JLabel lblname, labeladdress;
+    Choice meternumber, cmonth;
     CalculateBill() {
-
-
-        setSize(700,500);
-        setLocation(400,150);
+        setSize(700, 500);
+        setLocation(400, 150);
 
         JPanel p = new JPanel();
         p.setLayout(null);
-        p.setBackground(new Color(173,216,230));
-
+        p.setBackground(new Color(173, 216, 230));
         add(p);
 
-
         JLabel heading = new JLabel("Calculate Electricity Bill");
-        heading.setBounds(150,10,400,25);
-        heading.setFont(new Font("Tahoma",Font.PLAIN,24));
+        heading.setBounds(100, 10, 400, 25);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 24));
         p.add(heading);
 
         JLabel lblmeternumber = new JLabel("Meter Number");
-        lblmeternumber.setBounds(100,80,100,20);
+        lblmeternumber.setBounds(100, 80, 100, 20);
         p.add(lblmeternumber);
 
         meternumber = new Choice();
 
         try {
-            Conn c = new Conn();
+            Conn c  = new Conn();
             ResultSet rs = c.s.executeQuery("select * from customer");
             while(rs.next()) {
                 meternumber.add(rs.getString("meter_no"));
             }
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        meternumber.setBounds(240,80,200,20);
+
+        meternumber.setBounds(240, 80, 200, 20);
         p.add(meternumber);
 
         JLabel lblmeterno = new JLabel("Name");
-        lblmeterno.setBounds(100,120,100,20);
+        lblmeterno.setBounds(100, 120, 100, 20);
         p.add(lblmeterno);
 
-        lblname = new JLabel(" ");
-        lblname.setBounds(240,120,100,20);
+        lblname = new JLabel("");
+        lblname.setBounds(240, 120, 100, 20);
         p.add(lblname);
 
-
         JLabel lbladdress = new JLabel("Address");
-        lbladdress.setBounds(100,160,100,20);
+        lbladdress.setBounds(100, 160, 100, 20);
         p.add(lbladdress);
 
         labeladdress = new JLabel();
-        labeladdress.setBounds(240,160,200,20);
+        labeladdress.setBounds(240, 160, 200, 20);
         p.add(labeladdress);
 
-        try{
+        try {
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '"+meternumber.getSelectedItem()+"'");
-            while (rs.next()) {
+            while(rs.next()) {
                 lblname.setText(rs.getString("name"));
                 labeladdress.setText(rs.getString("address"));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         meternumber.addItemListener(new ItemListener() {
-            @Override
             public void itemStateChanged(ItemEvent ie) {
-                try{
+                try {
                     Conn c = new Conn();
                     ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '"+meternumber.getSelectedItem()+"'");
-                    while (rs.next()) {
+                    while(rs.next()) {
                         lblname.setText(rs.getString("name"));
                         labeladdress.setText(rs.getString("address"));
                     }
-                }catch(Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
 
         JLabel lblcity = new JLabel("Units Consumed");
-        lblcity.setBounds(100,200,100,20);
+        lblcity.setBounds(100, 200, 100, 20);
         p.add(lblcity);
 
         tfunits = new JTextField();
-        tfunits.setBounds(240,200,200,20);
+        tfunits.setBounds(240, 200, 200, 20);
         p.add(tfunits);
 
-
         JLabel lblstate = new JLabel("Month");
-        lblstate.setBounds(100,240,100,20);
+        lblstate.setBounds(100, 240, 100, 20);
         p.add(lblstate);
 
         cmonth = new Choice();
-        cmonth.setBounds(240,240,200,20);
+        cmonth.setBounds(240, 240, 200, 20);
         cmonth.add("January");
-        cmonth.add("Feburary");
+        cmonth.add("February");
         cmonth.add("March");
         cmonth.add("April");
         cmonth.add("May");
@@ -129,54 +124,37 @@ public class CalculateBill extends JFrame implements ActionListener {
         cmonth.add("December");
         p.add(cmonth);
 
-
-
-
-
         next = new JButton("Submit");
-        next.setBounds(120,390,100,25);
+        next.setBounds(120, 350, 100,25);
         next.setBackground(Color.BLACK);
         next.setForeground(Color.WHITE);
         next.addActionListener(this);
         p.add(next);
 
         cancel = new JButton("Cancel");
-        cancel.setBounds(250,390,100,25);
+        cancel.setBounds(250, 350, 100,25);
         cancel.setBackground(Color.BLACK);
         cancel.setForeground(Color.WHITE);
         cancel.addActionListener(this);
         p.add(cancel);
 
-
-
         setLayout(new BorderLayout());
-        add(p , "Center");
+
+        add(p, "Center");
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/hicon2.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(150,300,Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(150, 300, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        add(image , "West");
-
+        add(image, "West");
 
         getContentPane().setBackground(Color.WHITE);
 
-
-
         setVisible(true);
-
-    }
-    public static void main(String[] args) {
-
-        new CalculateBill();
-
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-
-        if(e.getSource() == next) {
-
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
             String meter = meternumber.getSelectedItem();
             String units = tfunits.getText();
             String month = cmonth.getSelectedItem();
@@ -184,46 +162,41 @@ public class CalculateBill extends JFrame implements ActionListener {
             int totalbill = 0;
             int unit_consumed = Integer.parseInt(units);
 
-
             String query = "select * from tax";
 
-            try{
-
+            try {
                 Conn c = new Conn();
                 ResultSet rs = c.s.executeQuery(query);
 
-
-                while(rs.next()){
+                while(rs.next()) {
                     totalbill += unit_consumed * Integer.parseInt(rs.getString("cost_per_unit"));
                     totalbill += Integer.parseInt(rs.getString("meter_rent"));
                     totalbill += Integer.parseInt(rs.getString("service_charge"));
                     totalbill += Integer.parseInt(rs.getString("service_tax"));
                     totalbill += Integer.parseInt(rs.getString("swacch_bharat_cess"));
                     totalbill += Integer.parseInt(rs.getString("fixed_tax"));
-
                 }
-
-
-            } catch(Exception ae) {
-                ae.printStackTrace();
-
-
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            String query2 = "insert into bill values('"+meter+"','"+month+"','"+units+"','"+totalbill+"','Not Paid',)";
+
+            String query2 = "insert into bill values('"+meter+"', '"+month+"', '"+units+"', '"+totalbill+"', 'Not Paid')";
 
             try {
-                    Conn c = new Conn();
-                    c.s.executeUpdate(query2);
+                Conn c  =  new Conn();
+                c.s.executeUpdate(query2);
 
-
-                    JOptionPane.showMessageDialog(null,"Customer Bill Updated Successfully");
-                    setVisible(false);
-            }catch(Exception ie) {
-                ie.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Customer Bill Updated Successfully");
+                setVisible(false);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } else {
             setVisible(false);
-
         }
+    }
+
+    public static void main(String[] args) {
+        new CalculateBill();
     }
 }
