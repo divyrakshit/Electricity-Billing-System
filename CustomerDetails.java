1a08e4ddd4187da9657d12cabcab8d9601560642
divyrakshit <divyrakshit07@gmail.com>
2024-06-28 12:50:00 +0530

    Update CustomerDetails.java

--- CustomerDetails.java
+++ CustomerDetails.java
@@ -10,61 +10,51 @@ import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import net.proteanit.sql.DbUtils;
 
-public class CustomerDetails extends JFrame implements ActionListener {
 
-    Choice meternumber , cmonth;
+
+public class CustomerDetails extends JFrame implements ActionListener{
+
+    Choice meternumber, cmonth;
     JTable table;
-    JButton search,print;
-    CustomerDetails() {
+    JButton search, print;
+
+    CustomerDetails(){
         super("Customer Details");
 
-        setSize(1200,650);
-        setLocation(200,150);
-
-
-
+        setSize(1200, 650);
+        setLocation(200, 150);
 
         table = new JTable();
 
-        try{
+        try {
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from customer");
 
             table.setModel(DbUtils.resultSetToTableModel(rs));
-
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
 
         JScrollPane sp = new JScrollPane(table);
         add(sp);
 
-
-
         print = new JButton("Print");
         print.addActionListener(this);
-        add(print , "South");
-
+        add(print, "South");
 
 
         setVisible(true);
-
     }
 
-    public static void main(String[] args) {
-
-        new CustomerDetails();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-            try {
-                table.print();
-            }catch(Exception ae) {
-                ae.printStackTrace();
-            }
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            table.print();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    public static void main(String[] args) {
+        new CustomerDetails();
+    }
+}
